caf931bbb8c2acc0cf05ef640ab84c750faed78b
Kemal Maulana <kemskems12@gmail.com>
2013-07-14 17:18:07 +0800

    Fix test script

--- test.sh
+++ test.sh
@@ -5,17 +5,16 @@ CN2TW=tests/cn2tw
 
 # Initialization
 init() {
-	if [ -e ZHMain.class ]; then
-		rm ZHMain.class
-	fi
+	echo -n "Compiling... "
 	javac ZHMain.java
+	echo "OK"
 }
 
 # Test traditional to simplified Chinese conversion
 tw2cn_test() {
 	echo -n "Testing zh-TW to zh-CN conversion... "
 	java ZHMain zh-CN < $TW2CN/in > $TW2CN/tmp.out 2> /dev/null
-	DIFF=`diff $TW2CN/out $TW2CN/tmp.out`
+	DIFF=`diff -q $TW2CN/out $TW2CN/tmp.out`
 	if [ -z "$DIFF" ]; then
 		echo "SUCCESS"
 	else
@@ -28,7 +27,7 @@ tw2cn_test() {
 cn2tw_test() {
 	echo -n "Testing zh-CN to zh-TW conversion... "
 	java ZHMain zh-TW < $CN2TW/in > $CN2TW/tmp.out 2> /dev/null
-	DIFF=`diff $CN2TW/out $CN2TW/tmp.out`
+	DIFF=`diff -q $CN2TW/out $CN2TW/tmp.out`
 	if [ -z "$DIFF" ]; then
 		echo "SUCCESS"
 	else
