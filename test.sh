fb3696255a6ef14272b6aa264340782d50891b3a
Kemal Maulana <kemskems12@gmail.com>
2013-07-23 13:57:53 +0800

    Indent using spaces

--- test.sh
+++ test.sh
@@ -5,35 +5,35 @@ CN2TW=tests/cn2tw
 
 # Initialization
 init() {
-	echo -n "Compiling... "
-	javac ZHMain.java
-	echo "OK"
+    echo -n "Compiling... "
+    javac ZHMain.java
+    echo "OK"
 }
 
 # Test traditional to simplified Chinese conversion
 tw2cn_test() {
-	echo -n "Testing zh-TW to zh-CN conversion... "
-	java ZHMain zh-CN < $TW2CN/in > $TW2CN/tmp.out 2> /dev/null
-	DIFF=`diff -q $TW2CN/out $TW2CN/tmp.out`
-	if [ -z "$DIFF" ]; then
-		echo "SUCCESS"
-	else
-		echo "FAILED!"
-	fi
-	rm $TW2CN/tmp.out
+    echo -n "Testing zh-TW to zh-CN conversion... "
+    java ZHMain zh-CN < $TW2CN/in > $TW2CN/tmp.out 2> /dev/null
+    DIFF=`diff -q $TW2CN/out $TW2CN/tmp.out`
+    if [ -z "$DIFF" ]; then
+        echo "SUCCESS"
+    else
+        echo "FAILED!"
+    fi
+    rm $TW2CN/tmp.out
 }
 
 # Test simplified to traditional Chinese conversion
 cn2tw_test() {
-	echo -n "Testing zh-CN to zh-TW conversion... "
-	java ZHMain zh-TW < $CN2TW/in > $CN2TW/tmp.out 2> /dev/null
-	DIFF=`diff -q $CN2TW/out $CN2TW/tmp.out`
-	if [ -z "$DIFF" ]; then
-		echo "SUCCESS"
-	else
-		echo "FAILED!"
-	fi
-	rm $CN2TW/tmp.out
+    echo -n "Testing zh-CN to zh-TW conversion... "
+    java ZHMain zh-TW < $CN2TW/in > $CN2TW/tmp.out 2> /dev/null
+    DIFF=`diff -q $CN2TW/out $CN2TW/tmp.out`
+    if [ -z "$DIFF" ]; then
+        echo "SUCCESS"
+    else
+        echo "FAILED!"
+    fi
+    rm $CN2TW/tmp.out
 }
 
 init
